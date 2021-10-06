23d44d0c2a967bf2969039c0fad069038f245f75
“Cody <cmccarty@catalyte.io>
2021-10-06 17:24:02 -0500

    added react front end

--- springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -4,12 +4,14 @@ import com.example.springbootbackend.entity.Employee;
 import com.example.springbootbackend.repository.EmployeeRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:3000/")
 public class EmployeeController {
 
   @Autowired
